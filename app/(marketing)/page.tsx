d2fd2a3216e0e78c214a76173b4aa52f3eeefb37
b.petya <bedfordpetya@gmail.com>
2024-01-07 18:59:31 +0000

    Add custom and Google font

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,29 +1,52 @@
 import Link from "next/link";
+import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 import { Medal } from "lucide-react";
+
+import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 
-const MarketingPage = ()=>{
+//add fonts
+//custom font
+const headingFont = localFont({
+    src: "../../public/fonts/font.woff2",
+});
+
+//google font
+const textFont = Poppins({
+    subsets: ["latin"],
+    weight: ['400', '500', '600', '700', '800', '900'],
+});
+
+
+const MarketingPage = () => {
     return (
         <div className="flex items-center justify-center flex-col">
-            <div className="flex items-center justify-center flex-col">
+            <div className={cn(
+                "flex items-center justify-center flex-col",
+                headingFont.className
+            )}>
                 <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
                     <Medal className="h-6 w-6 mr-2" />
                     №1 task manager
                 </div>
-            <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
-            Task-Board Fuels Your Workflow
-            </h1>
-            <div className="text-3xl md:text-6xl text-center bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
-               Moving Work Forward
+                <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
+                    Task-Board Fuels Your Workflow
+                </h1>
+                <div className="text-3xl md:text-6xl text-center bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md pb-4 w-fit">
+                    Moving Work Forward
+                </div>
             </div>
-            </div>
-            <div className="text-small md:text-xl text-neutral-500 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
-            Elevate Collaboration, Project Management, and Productivity to New Heights. 
-            Whether in bustling office or the comfort of your home, Task-Board is the key to unlocking your distinctive workflow achievements
+            <div className={cn(
+                "text-small md:text-xl text-neutral-500 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+                textFont.className
+            )}>
+                Elevate Collaboration, Project Management, and Productivity to New Heights.
+                Whether in bustling office or the comfort of your home, Task-Board is the key to unlocking your distinctive workflow achievements
             </div>
             <Button className="mt-6" size="lg" asChild>
                 <Link href="/sign-up">
-                 Get Task-Board for free
+                    Get Task-Board for free
                 </Link>
             </Button>
         </div>

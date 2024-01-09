317adbd077c4a2575224cea6a5d196734e56c4f7
b.petya <bedfordpetya@gmail.com>
2024-01-09 21:51:56 +0000

    add site config for metadata

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+    name: 'Task-Board Manager',
+    description: 'Collaborate with your team, manage your projects, and store your files, all in one place.',
+     url: 'https://bpetya.me',
+    // twitter: '@mysite',
+    // image: 'https://mysite.com/images/og.png',
+
+    // Optional
+    // googleAnalyticsId: 'UA-XXXXXX-X',
+    
+}

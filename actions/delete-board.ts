c08595e17b10abd0c7b87ca9b321338fdfba86dd
Petya Angelova <bedfordpetya@gmail.com>
2024-01-21 12:44:04 +0200

    Add delete board functionality

---
+++ actions/delete-board.ts
@@ -0,0 +1,14 @@
+"use server";
+
+import { db } from "@/lib/db";
+import { revalidatePath } from "next/cache";
+
+export async function deleteBoard(id: string) {
+  await db.board.delete({
+    where: {
+      id,
+    },
+  });
+
+  revalidatePath("/organization/org_2bu9SLSVS6Xyki1i68OphUfTQq3");
+}
--- app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/board.tsx
@@ -1,3 +1,4 @@
+import { deleteBoard } from "@/actions/delete-board";
 import { Button } from "@/components/ui/button";
 
 interface BoardProps {
@@ -6,9 +7,25 @@ interface BoardProps {
 }
 
 const Board = ({ title, id }: BoardProps) => {
+  const deleteBoardWithId = deleteBoard.bind(null, id);
+
   return (
-    <form className="flex items-center gap-x-2">
+    <form action={deleteBoardWithId} className="flex items-center gap-x-2">
       <p>Board title: {title}</p>
+      <Button type="submit" variant={"destructive"} size={"sm"}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4 relative"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 1a9 9 0 100 18 9 9 0 000-18zM5 10a1 1 0 011-1h8a1 1 0 010 2H6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </Button>
     </form>
   );
 };

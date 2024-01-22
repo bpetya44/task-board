80b6d35fb18943a54a7b92a5d30fcfd31e4e5dfb
Petya Angelova <bedfordpetya@gmail.com>
2024-01-22 17:07:23 +0200

    Refactor create-board action and add form validation

--- actions/create-board.ts
+++ actions/create-board.ts
@@ -2,21 +2,49 @@
 
 import { db } from "@/lib/db";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 import { z } from "zod";
 
+export type State = {
+  errors?: {
+    title?: string[];
+  };
+  message?: string | null;
+};
+
 const CreateBoard = z.object({
-  title: z.string(),
+  title: z
+    .string()
+    .min(3, { message: "Title must be at least 3 characters" })
+    .max(255, { message: "Title must be at most 255 characters" }),
 });
 
-export async function create(formData: FormData) {
-  const { title } = CreateBoard.parse({
+export async function create(prevState: State, formData: FormData) {
+  const validatedFields = CreateBoard.safeParse({
     title: formData.get("title"),
   });
 
-  await db.board.create({
-    data: {
-      title,
-    },
-  });
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: "Missing required fields",
+    };
+  }
+
+  const { title } = validatedFields.data;
+
+  try {
+    await db.board.create({
+      data: {
+        title,
+      },
+    });
+  } catch (err) {
+    return {
+      message: "Failed to create board",
+    };
+  }
+
   revalidatePath("/organization/org_2bu9SLSVS6Xyki1i68OphUfTQq3");
+  redirect("/organization/org_2bu9SLSVS6Xyki1i68OphUfTQq3");
 }
--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -2,22 +2,14 @@ import { create } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
 import { db } from "@/lib/db";
 import Board from "./board";
+import { Form } from "./form";
 
 const OrganizationIdPage = async () => {
   const boards = await db.board.findMany();
 
   return (
     <div className="flex flex-col space-y-4">
-      <form action={create} className="flex px-2 gap-1">
-        <input
-          id="title"
-          name="title"
-          required
-          placeholder="Enter a title"
-          className="border-2 border-gray-300 rounded-sm p-2 "
-        />
-        <Button type="submit">Submit</Button>
-      </form>
+      <Form />
 
       <div className="space-y-2">
         {boards.map((board) => (

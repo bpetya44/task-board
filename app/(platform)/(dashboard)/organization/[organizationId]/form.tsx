509900c0978017d3f2f4cf8e99c5ddca6336b263
Petya Angelova <bedfordpetya@gmail.com>
2024-01-23 17:49:52 +0200

    Add form components for board deletion and input

--- app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -3,6 +3,8 @@
 import { create } from "@/actions/create-board";
 import { Button } from "@/components/ui/button";
 import { useFormState } from "react-dom";
+import { FormInput } from "./fotm-input";
+import { FormButton } from "./form-button";
 
 export const Form = () => {
   const initialState = { message: null, errors: {} };
@@ -12,24 +14,9 @@ export const Form = () => {
   return (
     <form action={dispatch}>
       <div className="flex flex-col space-y-2">
-        <input
-          id="title"
-          name="title"
-          required
-          placeholder="Enter a title"
-          className="border-2 border-gray-300 rounded-sm p-2 "
-        />
-        {state?.errors?.title ? (
-          <div>
-            {state.errors.title.map((error: string) => (
-              <p key={error} className="text-red-500 text-sm">
-                {error}
-              </p>
-            ))}
-          </div>
-        ) : null}
+        <FormInput errors={state?.errors} />
       </div>
-      <Button type="submit">Submit</Button>
+      <FormButton />
     </form>
   );
 };

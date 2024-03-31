7001872f56f322c1da436558b113cf992d00fd98
b.petya <bedfordpetya@gmail.com>
2024-03-31 06:21:33 +0000

    Add create function to OrganizationIdPage component

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,30 @@
+import { db } from "@/lib/db";
 
 
 const OrganizationIdPage = () => {
+    async function create(formData: FormData){
+        "use server";
+        const title = formData.get("title") as string;
+
+        await db.board.create({
+            data: {
+                title,
+            }
+        });
+    }
+
+
     return (
         <div>
-       Organization Page
+            <form>
+                <input 
+                    id="title" 
+                    name="title" 
+                    required
+                    placeholder="Enter a title"
+                    className="border-2 border-gray-300 rounded-sm p-2"
+                />
+            </form>
         </div>
     );
     }
